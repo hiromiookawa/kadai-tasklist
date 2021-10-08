9e15af5a93a7a1d8172018820d45cddba6de0122
81805 <81805@192.168.11.18>
2021-10-08 21:15:42 +0900

    tasklist

--- src/models/Task.java
+++ src/models/Task.java
@@ -7,9 +7,22 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Task AS m ORDER BY m.id DESC"
+        ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(m) FROM Task AS m"
+            )
+
+})
 @Table(name = "tasks")
 public class Task {
     @Id
@@ -17,14 +30,14 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(name = "content", length = 255, nullable = false)
+    private String content;
+
     @Column(name = "created_at", nullable = false)
     private Timestamp created_at;
 
     @Column(name = "updated_at", nullable = false)
     private Timestamp updated_at;
-
-    @Column(name = "content", length = 255, nullable = false)
-    private String content;
 
     public Integer getId() {
         return id;
@@ -32,6 +45,14 @@ public class Task {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public Timestamp getCreated_at() {
@@ -50,11 +71,5 @@ public class Task {
         this.updated_at = updated_at;
     }
 
-    public String getContent() {
-        return content;
-    }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
